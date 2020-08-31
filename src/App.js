41a7006c2b9c0ebc9a913514256dc098dd006ef7
Cristian1001001 <trevor.tm21@gmail.com>
2020-08-31 15:40:23 +0300

    connected firbase and sign in with google

--- src/App.js
+++ src/App.js
@@ -6,18 +6,42 @@ import HomePage from "./pages/homepage/homepages.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.componenr";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
+import {auth} from "./firebase/firebase.utils";
 
-function App() {
-  return (
-    <div>
-        <Header/>
-        <Switch>
-            <Route exact path='/' component={HomePage} />
-            <Route  path='/shop' component={ShopPage} />
-            <Route  path='/signIn' component={SignInAndSignUpPage} />
-        </Switch>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            currentUser: null
+        }
+    }
+    unsubscribeFromAuth = null;
+
+    componentDidMount() {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(user=>{
+            this.setState({currentUser: user});
+        })
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeFromAuth();
+    }
+
+    render() {
+
+
+        return (
+            <div>
+                <Header currentUser={this.state.currentUser}/>
+                <Switch>
+                    <Route exact path='/' component={HomePage}/>
+                    <Route path='/shop' component={ShopPage}/>
+                    <Route path='/signIn' component={SignInAndSignUpPage}/>
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
