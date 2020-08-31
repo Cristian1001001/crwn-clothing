0a78d68992cc953b8c644fd33337417390d9eae3
Cristian1001001 <trevor.tm21@gmail.com>
2020-08-31 11:30:02 +0300

    sign up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom'
-import ShopPage from "./pages/shop/shop.component";
 import './App.css';
 
 import HomePage from "./pages/homepage/homepages.component";
+import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.componenr";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Switch>
             <Route exact path='/' component={HomePage} />
             <Route  path='/shop' component={ShopPage} />
+            <Route  path='/signIn' component={SignInAndSignUpPage} />
         </Switch>
     </div>
   );
